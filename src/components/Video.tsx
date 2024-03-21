726f7e8e950e61d5fdae0d88ecaaff1ddf809b1c
ZIMNF <znfirdaus7@gmail.com>
2024-03-22 00:35:49 +0700

    fix: video v2

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+
 const Video = () => {
   return (
     <>
       <div className="h-auto md:h-[85vh] w-auto flex justify-center pb-3 pt-20">
-        <video src={require("../../public/v2.webm")} autoPlay loop muted className="rounded-md" />
+        <Image src={require("../../public/v2.gif")} alt="abot" className="h-auto w-auto rounded-lg" />
+        {/* <video src={require("../../public/v2.webm")} autoPlay loop muted className="rounded-md" /> */}
       </div>
+
       <h1 className="text-3xl md:text-5xl text-center pb-4">
         Grab. Making <br /> Everyday better.
       </h1>
